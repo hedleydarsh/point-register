d47ff9cbc5a7fa59a85d90c7842df2c868452253
unknown <hedleydarsh>
2021-04-04 13:57:44 -0300

    Atualizando models users e collaborators e ajustando relacionamentos

--- app/Models/Collaborator.php
+++ app/Models/Collaborator.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Collaborator extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    //Acessor para retornar nomes sempre em letra maiúscula
+    public function getNameAttribute($value)
+    {
+        return strtoupper($value);
+    }
+
+
+    ////Acessor para retornar data de nascimento em formato brasileiro
+    public function getDtBirthAttribute($value)
+    {
+        $value = Carbon::createFromFormat('Y-m-d', $value)
+            ->format('d-m-Y');
+        return strtoupper($value);
+    }
 }
